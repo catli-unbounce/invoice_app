3a372af0b455d46e191661ede136ab0d0ed6dfe4
catli-unbounce <catherine.li@unbounce.com>
2022-02-27 09:03:03 -0800

    Style the stick navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import styles from './App.scss';
 function App() {
   return (
-    <div className="global-width">
-      <img src="/images/icon-check.svg" alt="image" />
-      <h1>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h1>
-      <h2>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h2>
-      <h3>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h3>
-      <h4>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h4>
-      <button className="danger">New Invoice</button>    
-      <span className="tag tag--paid">Pending</span>
-    </div>
+    <>
+      <div className="vertical-nav"></div>
+      <div className="global-width">
+        <img src="/images/icon-check.svg" alt="image" />
+        <h1>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h1>
+        <h2>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h2>
+        <h3>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h3>
+        <h4>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h4>
+        <button className="danger">New Invoice</button>    
+        <span className="tag tag--paid">Pending</span>
+      </div>
+    </>
   );
 }
 
