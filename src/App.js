a9772a5d6f5f9a848e6213c2e37d7bdeb00c80fa
catli-unbounce <catherine.li@unbounce.com>
2022-02-27 08:51:26 -0800

    Created tag elements

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import styles from './App.scss';
 function App() {
   return (
     <div className="global-width">
+      <img src="/images/icon-check.svg" alt="image" />
       <h1>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h1>
       <h2>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h2>
       <h3>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h3>
       <h4>m ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliqu</h4>
-      <button className={styles["primary"]}>New Invoice</button>    
+      <button className="danger">New Invoice</button>    
+      <span className="tag tag--paid">Pending</span>
     </div>
   );
 }
